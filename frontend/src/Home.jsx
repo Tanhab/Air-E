a693bce8c90befccdcda72dcc68a9bebc263e56d
tanhab <tanhab3006@gmail.com>
2023-11-03 12:51:44 +0600

    viewpoint boundary in useEffect

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -14,21 +14,40 @@ export default function Home() {
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
+  const [mapBounds, setMapBounds] = useState(null);
 
-  console.log(import.meta.env.VITE_MAPBOX_TOKEN)
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-    });
-  });
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [lng, lat],
+        zoom: zoom,
+      });
+
+      map.current.on("moveend", () => {
+        // When the map moves or zooms, update the map bounds
+        const bounds = map.current.getBounds();
+        console.log(bounds)
+        setMapBounds(bounds);
+      });
+    }
+  }, [lng, lat, zoom]);
   return (
     <>
       <Navbar />
 
       <div>
         <div style={{ height: "100vh" }} ref={mapContainer} />
+        {mapBounds && (
+          <div>
+            <p>Map Bounds:</p>
+            <p>Top Left (Latitude): {mapBounds.getNorth()}</p>
+            <p>Top Left (Longitude): {mapBounds.getWest()}</p>
+            <p>Bottom Right (Latitude): {mapBounds.getSouth()}</p>
+            <p>Bottom Right (Longitude): {mapBounds.getEast()}</p>
+          </div>
+        )}
       </div>
     </>
   );
