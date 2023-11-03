87bf8755464057456d5709545210b19779c900e1
Dristy03 <ananna49@student.sust.edu>
2023-11-03 16:57:20 +0600

    autocomplete search done

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,10 +2,14 @@ import React, { useRef, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { styled, useTheme } from "@mui/material/styles";
 import mapboxgl from "mapbox-gl";
-
+import styles from "./styles/Home.module.css";
 import Navbar from "./components/navbar";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import { InputAdornment } from "@mui/material";
 
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 export default function Home() {
   const theme = useTheme();
@@ -13,14 +17,15 @@ export default function Home() {
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(2);
   const [mapBounds, setMapBounds] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     if (!map.current) {
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v12",
+        style: "mapbox://styles/sagor60/cloialudf003j01prgw21f3jd",
         center: [lng, lat],
         zoom: zoom,
       });
@@ -28,18 +33,31 @@ export default function Home() {
       map.current.on("moveend", () => {
         // When the map moves or zooms, update the map bounds
         const bounds = map.current.getBounds();
-        console.log(bounds)
+        console.log(bounds);
         setMapBounds(bounds);
+      });
+
+      map.current.on("click", (e) => {
+        console.log("CLICKED");
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["country-fills"],
+        });
+        if (!features.length) return;
+        const { properties } = features[0];
+        const { property, description } = activeRef.current;
+        alert(`(${properties.name}) ${properties[property]} ${description}`);
       });
     }
   }, [lng, lat, zoom]);
+
+
   return (
     <>
       <Navbar />
 
-      <div>
-        <div style={{ height: "100vh" }} ref={mapContainer} />
-        {mapBounds && (
+      <div className={styles.container}>
+        <div className={styles.mapcontainer} ref={mapContainer} />
+        {/* {mapBounds && (
           <div>
             <p>Map Bounds:</p>
             <p>Top Left (Latitude): {mapBounds.getNorth()}</p>
@@ -47,7 +65,7 @@ export default function Home() {
             <p>Bottom Right (Latitude): {mapBounds.getSouth()}</p>
             <p>Bottom Right (Longitude): {mapBounds.getEast()}</p>
           </div>
-        )}
+        )} */}
       </div>
     </>
   );
